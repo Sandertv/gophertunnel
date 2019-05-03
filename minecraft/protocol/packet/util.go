603cc89fecdb67345e02c4717471f1ea94ffab28
Sander <st.ten.veldhuis@gmail.com>
2019-05-03 18:12:50 +0200

    ChainErr: No need to specifically check if the amount of errors passed is 0.

--- minecraft/protocol/packet/util.go
+++ minecraft/protocol/packet/util.go
@@ -8,9 +8,6 @@ import (
 // ChainErr chains together a variadic amount of errors into a single error and returns it. If all errors
 // passed are nil, the error returned will also be nil.
 func ChainErr(err ...error) error {
-	if len(err) == 0 {
-		return nil
-	}
 	var msg string
 	for _, e := range err {
 		if e == nil {
