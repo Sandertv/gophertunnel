51f8fd629dd067f9af84febebc599792fde74c1f
Sandertv <st.ten.veldhuis@gmail.com>
2019-08-27 20:27:02 +0200

    gophertunnel/minecraft: Added a new `text` package for text formatting related code.

---
+++ minecraft/text/colour.go
@@ -0,0 +1,153 @@
+package text
+
+import (
+	"fmt"
+	"strings"
+)
+
+// FormatFunc represents a function that may be called on a list of values to format the values and apply
+// a colour or format on them, such as making the text bold.
+// The formatting on the values passed is done using Minecraft formatting codes. To turn it into a string that
+// may be printed, use text.ANSI(string).
+type FormatFunc func(a ...interface{}) string
+
+// ANSI converts all Minecraft text formatting codes in the values passed to ANSI formatting codes, so that
+// it may be displayed properly in the terminal.
+func ANSI(a ...interface{}) string {
+	str := make([]string, len(a))
+	for i, v := range a {
+		str[i] = minecraftReplacer.Replace(fmt.Sprint(v))
+	}
+	return strings.Join(str, " ")
+}
+
+// Minecraft converts all ANSI text formatting codes in the values passed to Minecraft formatting codes, so
+// that it may be displayed in-game.
+func Minecraft(a ...interface{}) string {
+	str := make([]string, len(a))
+	for i, v := range a {
+		str[i] = ansiReplacer.Replace(fmt.Sprint(v))
+	}
+	return strings.Join(str, " ")
+}
+
+// Black returns a black formatter.
+func Black() FormatFunc {
+	return formatFunc(black, reset)
+}
+
+// DarkBlue returns a dark blue formatter.
+func DarkBlue() FormatFunc {
+	return formatFunc(darkBlue, reset)
+}
+
+// DarkGreen returns a dark green formatter.
+func DarkGreen() FormatFunc {
+	return formatFunc(darkGreen, reset)
+}
+
+// DarkAqua returns a dark aqua formatter.
+func DarkAqua() FormatFunc {
+	return formatFunc(darkAqua, reset)
+}
+
+// DarkRed returns a dark red formatter.
+func DarkRed() FormatFunc {
+	return formatFunc(darkRed, reset)
+}
+
+// DarkPurple returns a dark purple formatter.
+func DarkPurple() FormatFunc {
+	return formatFunc(darkPurple, reset)
+}
+
+// Gold returns a gold formatter.
+func Gold() FormatFunc {
+	return formatFunc(gold, reset)
+}
+
+// Grey returns a grey formatter.
+func Grey() FormatFunc {
+	return formatFunc(grey, reset)
+}
+
+// DarkGrey returns a dark grey formatter.
+func DarkGrey() FormatFunc {
+	return formatFunc(darkGrey, reset)
+}
+
+// Blue returns a blue formatter.
+func Blue() FormatFunc {
+	return formatFunc(blue, reset)
+}
+
+// Green returns a green formatter.
+func Green() FormatFunc {
+	return formatFunc(green, reset)
+}
+
+// Aqua returns an aqua formatter.
+func Aqua() FormatFunc {
+	return formatFunc(aqua, reset)
+}
+
+// Red returns a red formatter.
+func Red() FormatFunc {
+	return formatFunc(red, reset)
+}
+
+// Purple returns a purple formatter.
+func Purple() FormatFunc {
+	return formatFunc(purple, reset)
+}
+
+// Yellow returns a yellow formatter.
+func Yellow() FormatFunc {
+	return formatFunc(yellow, reset)
+}
+
+// White returns a white formatter.
+func White() FormatFunc {
+	return formatFunc(white, reset)
+}
+
+// Obfuscated returns an obfuscated formatter.
+func Obfuscated() FormatFunc {
+	return formatFunc(obfuscated, reset)
+}
+
+// Bold returns a bold formatter.
+func Bold() FormatFunc {
+	return formatFunc(bold, reset)
+}
+
+// Strikethrough returns a strikethrough formatter.
+func Strikethrough() FormatFunc {
+	return formatFunc(strikethrough, reset)
+}
+
+// Italic returns an italic formatter.
+func Italic() FormatFunc {
+	return formatFunc(italic, reset)
+}
+
+// formatFunc produces a FormatFunc for a given format code and reset code passed. It will start each of the
+// arguments passed to the FormatFunc with the format code and end with the reset code.
+func formatFunc(formatCode, reset string) FormatFunc {
+	return func(a ...interface{}) string {
+		str := &strings.Builder{}
+		str.WriteString(formatCode)
+		for i, v := range a {
+			if i != 0 {
+				str.WriteByte(' ')
+			}
+			str.WriteString(fmt.Sprint(v))
+			if i != len(a)-1 {
+				str.WriteString(formatCode)
+			} else {
+				str.WriteString(reset)
+			}
+		}
+		return str.String()
+	}
+}
---
+++ minecraft/text/map.go
@@ -0,0 +1,95 @@
+package text
+
+import "strings"
+
+const (
+	black      = "§0"
+	darkBlue   = "§1"
+	darkGreen  = "§2"
+	darkAqua   = "§3"
+	darkRed    = "§4"
+	darkPurple = "§5"
+	gold       = "§6"
+	grey       = "§7"
+	darkGrey   = "§8"
+	blue       = "§9"
+	green      = "§a"
+	aqua       = "§b"
+	red        = "§c"
+	purple     = "§d"
+	yellow     = "§e"
+	white      = "§f"
+
+	obfuscated    = "§k"
+	bold          = "§l"
+	strikethrough = "§m"
+	underline     = "§n"
+	italic        = "§o"
+	reset         = "§r"
+)
+
+const (
+	ansiBlack      = "\x1b[38;5;16m"
+	ansiDarkBlue   = "\x1b[38;5;19m"
+	ansiDarkGreen  = "\x1b[38;5;34m"
+	ansiDarkAqua   = "\x1b[38;5;37m"
+	ansiDarkRed    = "\x1b[38;5;124m"
+	ansiDarkPurple = "\x1b[38;5;127m"
+	ansiGold       = "\x1b[38;5;214m"
+	ansiGrey       = "\x1b[38;5;145m"
+	ansiDarkGrey   = "\x1b[38;5;59m"
+	ansiBlue       = "\x1b[38;5;63m"
+	ansiGreen      = "\x1b[38;5;83m"
+	ansiAqua       = "\x1b[38;5;87m"
+	ansiRed        = "\x1b[38;5;203m"
+	ansiPurple     = "\x1b[38;5;207m"
+	ansiYellow     = "\x1b[38;5;227m"
+	ansiWhite      = "\x1b[38;5;231m"
+
+	ansiObfuscated    = ""
+	ansiBold          = "\x1b[1m"
+	ansiStrikethrough = "\x1b[9m"
+	ansiUnderline     = "\x1b[4m"
+	ansiItalic        = "\x1b[3m"
+	ansiReset         = "\x1b[m"
+)
+
+var m = map[string]string{
+	black:         ansiBlack,
+	darkBlue:      ansiDarkBlue,
+	darkGreen:     ansiDarkGreen,
+	darkAqua:      ansiDarkAqua,
+	darkRed:       ansiDarkRed,
+	darkPurple:    ansiDarkPurple,
+	gold:          ansiGold,
+	grey:          ansiGrey,
+	darkGrey:      ansiDarkGrey,
+	blue:          ansiBlue,
+	green:         ansiGreen,
+	aqua:          ansiAqua,
+	red:           ansiRed,
+	purple:        ansiPurple,
+	yellow:        ansiYellow,
+	white:         ansiWhite,
+	obfuscated:    ansiObfuscated,
+	bold:          ansiBold,
+	strikethrough: ansiStrikethrough,
+	underline:     ansiUnderline,
+	reset:         ansiReset,
+}
+
+// minecraftReplacer and ansiReplacer are used to translate ANSI formatting codes to Minecraft formatting
+// codes and vice versa.
+var minecraftReplacer, ansiReplacer *strings.Replacer
+
+func init() {
+	var minecraftToANSI []string
+	var ansiToMinecraft []string
+	for minecraftCode, ansiCode := range m {
+		if ansiCode != ansiObfuscated {
+			ansiToMinecraft = append(ansiToMinecraft, ansiCode, minecraftCode)
+		}
+		minecraftToANSI = append(minecraftToANSI, minecraftCode, ansiCode)
+	}
+	minecraftReplacer, ansiReplacer = strings.NewReplacer(minecraftToANSI...), strings.NewReplacer(ansiToMinecraft...)
+}
