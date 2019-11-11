766d87c4119c6d2c0f1eac86d1ef5883b68d2f5b
Sandertv <st.ten.veldhuis@gmail.com>
2019-11-11 16:42:01 +0100

    packet.SettingsCommands: Improved and fixed some documentation.

--- minecraft/protocol/packet/settings_command.go
+++ minecraft/protocol/packet/settings_command.go
@@ -6,13 +6,15 @@ import (
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
 )
 
-// SettingsCommand is sent by the client when it sends a command that changes settings. The actual use of this
-// packet is unknown.
+// SettingsCommand is sent by the client when it changes a setting in the settings that results in the issuing
+// of a command to the server, such as when Show Coordinates is enabled.
 type SettingsCommand struct {
-	// CommandLine is the full command line that the client sent to update a setting.
+	// CommandLine is the full command line that was sent to the server as a result of the setting that the
+	// client changed.
 	CommandLine string
 	// SuppressOutput specifies if the client requests the suppressing of the output of the command that was
-	// executed.
+	// executed. Generally this is set to true, as the client won't need a message to confirm the output of
+	// the change.
 	SuppressOutput bool
 }
 
