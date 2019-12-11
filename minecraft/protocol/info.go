44f0bf77dfc8b5e64004a41d9f0accd2556e7ce6
Sandertv <st.ten.veldhuis@gmail.com>
2019-12-11 20:13:31 +0100

    minecraft/protocol: Updated gophertunnel to 1.14.

--- minecraft/protocol/info.go
+++ minecraft/protocol/info.go
@@ -2,7 +2,7 @@ package protocol
 
 const (
 	// CurrentProtocol is the current protocol version for the version below.
-	CurrentProtocol = 388
+	CurrentProtocol = 389
 	// CurrentVersion is the current version of Minecraft as supported by the `packet` package.
-	CurrentVersion = "1.13.1"
+	CurrentVersion = "1.14.0"
 )
