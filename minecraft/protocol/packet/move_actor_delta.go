943248ad43e0c0ae4828be834c4074de2d99223e
Sandertv <st.ten.veldhuis@gmail.com>
2020-03-18 08:26:22 +0100

    move_actor_delta.go: Fixed encoding of the packet. Previously this packet had a 'DeltaRotation' field in gophertunnel, but this is not correct. The rotation written is in fact an absolute rotation, but it is still only written if the flags are set.

--- minecraft/protocol/packet/move_actor_delta.go
+++ minecraft/protocol/packet/move_actor_delta.go
@@ -9,26 +9,31 @@ import (
 )
 
 const (
-	moveFlagHasX = 1 << iota
-	moveFlagHasY
-	moveFlagHasZ
-	moveFlagHasRotX
-	moveFlagHasRotY
-	moveFlagHasRotZ
+	MoveActorDeltaFlagHasX = 1 << iota
+	MoveActorDeltaFlagHasY
+	MoveActorDeltaFlagHasZ
+	MoveActorDeltaFlagHasRotX
+	MoveActorDeltaFlagHasRotY
+	MoveActorDeltaFlagHasRotZ
+	MoveActorDeltaFlagOnGround
+	MoveActorDeltaFlagTeleport
+	MoveActorDeltaFlagForceMove
 )
 
 // MoveActorDelta is sent by the server to move an entity by a given delta. The packet is specifically
 // optimised to save as much space as possible, by only writing non-zero fields.
 type MoveActorDelta struct {
+	// Flags is a list of flags that specify what data is in the packet
+	Flags uint16
 	// EntityRuntimeID is the runtime ID of the entity that is being moved. The packet works provided a
 	// non-player entity with this runtime ID is present.
 	EntityRuntimeID uint64
 	// DeltaPosition is the difference from the previous position to the new position. It is the distance on
 	// each axis that the entity should be moved.
 	DeltaPosition mgl32.Vec3
-	// DeltaRotation is the difference from the previous rotation to the new rotation. It is the rotation on
-	// each axis that the entity should be turned.
-	DeltaRotation mgl32.Vec3
+	// Rotation is the new absolute rotation. Unlike the position, it is not actually a delta. If any of the
+	// values of this rotation are not sent, these values are 0 and no flag for them is present.
+	Rotation mgl32.Vec3
 }
 
 // ID ...
@@ -39,94 +44,75 @@ func (*MoveActorDelta) ID() uint32 {
 // Marshal ...
 func (pk *MoveActorDelta) Marshal(buf *bytes.Buffer) {
 	_ = protocol.WriteVaruint64(buf, pk.EntityRuntimeID)
-	var flags uint16
-	if pk.DeltaPosition[0] != 0 {
-		flags |= moveFlagHasX
-	}
-	if pk.DeltaPosition[1] != 0 {
-		flags |= moveFlagHasY
-	}
-	if pk.DeltaPosition[2] != 0 {
-		flags |= moveFlagHasZ
-	}
-	if pk.DeltaRotation[0] != 0 {
-		flags |= moveFlagHasRotX
-	}
-	if pk.DeltaRotation[1] != 0 {
-		flags |= moveFlagHasRotY
-	}
-	if pk.DeltaRotation[2] != 0 {
-		flags |= moveFlagHasRotZ
-	}
-	_ = binary.Write(buf, binary.LittleEndian, flags)
-	if pk.DeltaPosition[0] != 0 {
+	_ = binary.Write(buf, binary.LittleEndian, pk.Flags)
+	if pk.Flags&MoveActorDeltaFlagHasX != 0 {
 		_ = protocol.WriteVarint32(buf, int32(math.Float32bits(pk.DeltaPosition[0])))
 	}
-	if pk.DeltaPosition[1] != 0 {
+	if pk.Flags&MoveActorDeltaFlagHasY != 0 {
 		_ = protocol.WriteVarint32(buf, int32(math.Float32bits(pk.DeltaPosition[1])))
 	}
-	if pk.DeltaPosition[2] != 0 {
+	if pk.Flags&MoveActorDeltaFlagHasZ != 0 {
 		_ = protocol.WriteVarint32(buf, int32(math.Float32bits(pk.DeltaPosition[2])))
 	}
-	if pk.DeltaRotation[0] != 0 {
-		_ = binary.Write(buf, binary.LittleEndian, byte(pk.DeltaRotation[0]/(360.0/256.0)))
+	if pk.Flags&MoveActorDeltaFlagHasRotX != 0 {
+		_ = binary.Write(buf, binary.LittleEndian, byte(float32(math.Mod(float64(pk.Rotation[0]/(360.0/256.0)), 360))))
 	}
-	if pk.DeltaRotation[1] != 0 {
-		_ = binary.Write(buf, binary.LittleEndian, byte(pk.DeltaRotation[1]/(360.0/256.0)))
+	if pk.Flags&MoveActorDeltaFlagHasRotY != 0 {
+		_ = binary.Write(buf, binary.LittleEndian, byte(float32(math.Mod(float64(pk.Rotation[1]/(360.0/256.0)), 360))))
 	}
-	if pk.DeltaRotation[2] != 0 {
-		_ = binary.Write(buf, binary.LittleEndian, byte(pk.DeltaRotation[2]/(360.0/256.0)))
+	if pk.Flags&MoveActorDeltaFlagHasRotZ != 0 {
+		_ = binary.Write(buf, binary.LittleEndian, byte(float32(math.Mod(float64(pk.Rotation[2]/(360.0/256.0)), 360))))
 	}
 }
 
 // Unmarshal ...
 func (pk *MoveActorDelta) Unmarshal(buf *bytes.Buffer) error {
 	pk.DeltaPosition = mgl32.Vec3{}
-	pk.DeltaRotation = mgl32.Vec3{}
-	var flags uint16
+	pk.Rotation = mgl32.Vec3{}
+
 	if err := chainErr(
 		protocol.Varuint64(buf, &pk.EntityRuntimeID),
-		binary.Read(buf, binary.LittleEndian, &flags),
+		binary.Read(buf, binary.LittleEndian, &pk.Flags),
 	); err != nil {
 		return err
 	}
 	var v int32
-	if flags&moveFlagHasX != 0 {
+	if pk.Flags&MoveActorDeltaFlagHasX != 0 {
 		if err := protocol.Varint32(buf, &v); err != nil {
 			return err
 		}
 		pk.DeltaPosition[0] = math.Float32frombits(uint32(v))
 	}
-	if flags&moveFlagHasY != 0 {
+	if pk.Flags&MoveActorDeltaFlagHasY != 0 {
 		if err := protocol.Varint32(buf, &v); err != nil {
 			return err
 		}
 		pk.DeltaPosition[1] = math.Float32frombits(uint32(v))
 	}
-	if flags&moveFlagHasZ != 0 {
+	if pk.Flags&MoveActorDeltaFlagHasZ != 0 {
 		if err := protocol.Varint32(buf, &v); err != nil {
 			return err
 		}
 		pk.DeltaPosition[2] = math.Float32frombits(uint32(v))
 	}
 	var w byte
-	if flags&moveFlagHasRotX != 0 {
+	if pk.Flags&MoveActorDeltaFlagHasRotX != 0 {
 		if err := binary.Read(buf, binary.LittleEndian, &w); err != nil {
 			return err
 		}
-		pk.DeltaRotation[0] = float32(w) * (360.0 / 256.0)
+		pk.Rotation[0] = float32(w) * (360.0 / 256.0)
 	}
-	if flags&moveFlagHasRotY != 0 {
+	if pk.Flags&MoveActorDeltaFlagHasRotY != 0 {
 		if err := binary.Read(buf, binary.LittleEndian, &w); err != nil {
 			return err
 		}
-		pk.DeltaRotation[1] = float32(w) * (360.0 / 256.0)
+		pk.Rotation[1] = float32(w) * (360.0 / 256.0)
 	}
-	if flags&moveFlagHasRotZ != 0 {
+	if pk.Flags&MoveActorDeltaFlagHasRotZ != 0 {
 		if err := binary.Read(buf, binary.LittleEndian, &w); err != nil {
 			return err
 		}
-		pk.DeltaRotation[2] = float32(w) * (360.0 / 256.0)
+		pk.Rotation[2] = float32(w) * (360.0 / 256.0)
 	}
 	return nil
 }
