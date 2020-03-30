fb4d6747e4adaa7e58be89f24697c49b9011171e
Sandertv <st.ten.veldhuis@gmail.com>
2020-03-30 14:39:24 +0200

    packet/container_close.go: Fixed documentation, which did not mention this packet was also sent by the client.

--- minecraft/protocol/packet/container_close.go
+++ minecraft/protocol/packet/container_close.go
@@ -6,7 +6,8 @@ import (
 )
 
 // ContainerClose is sent by the server to close a container the player currently has opened, which was opened
-// using the ContainerOpen packet.
+// using the ContainerOpen packet, or by the client to tell the server it closed a particular container, such
+// as the crafting grid.
 type ContainerClose struct {
 	// WindowID is the ID representing the window of the container that should be closed. It must be equal to
 	// the one sent in the ContainerOpen packet to close the designated window.
