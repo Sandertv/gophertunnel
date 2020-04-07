22e73a2dddb0b1933dc17a7352f32dda1a895a19
Sandertv <st.ten.veldhuis@gmail.com>
2020-04-07 19:10:54 +0200

    packet/spawn_particle_effect.go: Provided better documentation of the EntityUniqueID field.

--- minecraft/protocol/packet/spawn_particle_effect.go
+++ minecraft/protocol/packet/spawn_particle_effect.go
@@ -14,11 +14,13 @@ type SpawnParticleEffect struct {
 	// Dimension is the dimension that the particle is spawned in. Its exact usage is not clear, as the
 	// dimension has no direct effect on the particle.
 	Dimension byte
-	// EntityUniqueID is the unique ID of the entity that the spawned particle may be associated with. For
-	// a particle that isn't associated with an entity, this should be set to -1.
+	// EntityUniqueID is the unique ID of the entity that the spawned particle may be attached to. If this ID
+	// is not -1, the Position below will be interpreted as relative to the position of the entity associated
+	// with this unique ID.
 	EntityUniqueID int64
 	// Position is the position that the particle should be spawned at. If the position is too far away from
 	// the player, it will not show up.
+	// If EntityUniqueID is not -1, the position will be relative to the position of the entity.
 	Position mgl32.Vec3
 	// ParticleName is the name of the particle that should be shown. This name may point to a particle effect
 	// that is built-in, or to one implemented by behaviour packs.
