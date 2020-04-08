346a5548c86cf63230e22b914125215daff410d8
Sandertv <st.ten.veldhuis@gmail.com>
2020-04-08 10:27:58 +0200

    packet/encoder.go and decoder.go: Changed zlib to flate, bumped version to 1.16.

--- minecraft/protocol/info.go
+++ minecraft/protocol/info.go
@@ -2,9 +2,9 @@ package protocol
 
 const (
 	// CurrentProtocol is the current protocol version for the version below.
-	CurrentProtocol = 389
+	CurrentProtocol = 401
 	// CurrentVersion is the current version of Minecraft as supported by the `packet` package.
-	CurrentVersion = "1.14.0"
+	CurrentVersion = "1.16.0"
 	// CurrentBlockVersion is the current version of blocks (states) of the game. This version is composed
 	// of 4 bytes indicating a version, interpreted as a big endian int. The current version represents
 	// {1, 15, 0, 0}.
