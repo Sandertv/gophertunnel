6518a24d89251a451ebb38b1c85c8fd5c0d0a6f0
Sandertv <st.ten.veldhuis@gmail.com>
2020-06-22 10:26:12 +0200

    packet/item_stack_request.go: Fixed incorrect documentation.

--- minecraft/protocol/packet/item_stack_request.go
+++ minecraft/protocol/packet/item_stack_request.go
@@ -6,9 +6,9 @@ import (
 )
 
 // ItemStackRequest is sent by the client to change item stacks in an inventory. It is essentially a
-// replacement of the InventoryTransaction packet added in 1.16. Similarly to the InventoryTransaction packet,
-// it is sent for actions such as crafting and placing blocks, but also for actions restricted to inventories
-// such as moving items around.
+// replacement of the InventoryTransaction packet added in 1.16 for inventory specific actions, such as moving
+// items around or crafting. The InventoryTransaction packet is still used for actions such as placing blocks
+// and interacting with entities.
 type ItemStackRequest struct {
 	// Requests holds a list of item stack requests. These requests are all separate, but the client buffers
 	// the requests, so you might find multiple unrelated requests in this packet.
