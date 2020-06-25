a62db5b293f29985bdb38afa09b1126832d701bc
Sandertv <st.ten.veldhuis@gmail.com>
2020-06-25 16:18:20 +0200

    protocol/item_stack.go: Fixed typo.

--- minecraft/protocol/item_stack.go
+++ minecraft/protocol/item_stack.go
@@ -328,7 +328,7 @@ type TakeStackRequestAction struct {
 	transferStackRequestAction
 }
 
-// PlaceStakeRequestAction is sent by the client to the server to place x amount of items from one slot into
+// PlaceStackRequestAction is sent by the client to the server to place x amount of items from one slot into
 // another slot, such as when shift clicking an item in the inventory to move it around or when moving an item
 // in the cursor into a slot.
 type PlaceStackRequestAction struct {
