2caef10b7231c627ed1e6d9477781406b501a4d4
Sandertv <st.ten.veldhuis@gmail.com>
2020-09-13 12:54:30 +0200

    minecraft/conn.go: Removed unused getFrame function.

--- minecraft/conn.go
+++ minecraft/conn.go
@@ -21,7 +21,6 @@ import (
 	"io"
 	"log"
 	"net"
-	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -373,29 +372,6 @@ func (conn *Conn) Close() error {
 	conn.close()
 	_ = conn.Flush()
 	return conn.conn.Close()
-}
-
-func getFrame(skipFrames int) runtime.Frame {
-	// We need the frame at index skipFrames+2, since we never want runtime.Callers and getFrame
-	targetFrameIndex := skipFrames + 2
-
-	// Set size to targetFrameIndex+2 to ensure we have room for one more caller than we need
-	programCounters := make([]uintptr, targetFrameIndex+2)
-	n := runtime.Callers(0, programCounters)
-
-	frame := runtime.Frame{Function: "unknown"}
-	if n > 0 {
-		frames := runtime.CallersFrames(programCounters[:n])
-		for more, frameIndex := true, 0; more && frameIndex <= targetFrameIndex; frameIndex++ {
-			var frameCandidate runtime.Frame
-			frameCandidate, more = frames.Next()
-			if frameIndex == targetFrameIndex {
-				frame = frameCandidate
-			}
-		}
-	}
-
-	return frame
 }
 
 // LocalAddr returns the local address of the underlying connection.
