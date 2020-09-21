732470f833f7295188aaab9daf69c59e10df36ff
Sandertv <st.ten.veldhuis@gmail.com>
2020-09-21 20:39:22 +0200

    main.go: Split main function into another handleConn function and run it on another goroutine.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/pelletier/go-toml"
 	"github.com/sandertv/gophertunnel/minecraft"
 	"github.com/sandertv/gophertunnel/minecraft/auth"
+	"golang.org/x/oauth2"
 	"io/ioutil"
 	"log"
 	"os"
@@ -32,57 +33,61 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		conn := c.(*minecraft.Conn)
-		serverConn, err := minecraft.Dialer{
-			TokenSource: src,
-			ClientData:  conn.ClientData(),
-		}.Dial("raknet", config.Connection.RemoteAddress)
-		if err != nil {
+		go handleConn(c.(*minecraft.Conn), listener, config, src)
+	}
+}
+
+// handleConn handles a new incoming minecraft.Conn from the minecraft.Listener passed.
+func handleConn(conn *minecraft.Conn, listener *minecraft.Listener, config config, src oauth2.TokenSource) {
+	serverConn, err := minecraft.Dialer{
+		TokenSource: src,
+		ClientData:  conn.ClientData(),
+	}.Dial("raknet", config.Connection.RemoteAddress)
+	if err != nil {
+		panic(err)
+	}
+	var g sync.WaitGroup
+	g.Add(2)
+	go func() {
+		if err := conn.StartGame(serverConn.GameData()); err != nil {
 			panic(err)
 		}
-		var g sync.WaitGroup
-		g.Add(2)
-		go func() {
-			if err := conn.StartGame(serverConn.GameData()); err != nil {
-				panic(err)
-			}
-			g.Done()
-		}()
-		go func() {
-			if err := serverConn.DoSpawn(); err != nil {
-				panic(err)
-			}
-			g.Done()
-		}()
-		g.Wait()
+		g.Done()
+	}()
+	go func() {
+		if err := serverConn.DoSpawn(); err != nil {
+			panic(err)
+		}
+		g.Done()
+	}()
+	g.Wait()
 
-		go func() {
-			defer listener.Disconnect(conn, "connection lost")
-			defer serverConn.Close()
-			for {
-				pk, err := conn.ReadPacket()
-				if err != nil {
-					return
-				}
-				if err := serverConn.WritePacket(pk); err != nil {
-					return
-				}
+	go func() {
+		defer listener.Disconnect(conn, "connection lost")
+		defer serverConn.Close()
+		for {
+			pk, err := conn.ReadPacket()
+			if err != nil {
+				return
 			}
-		}()
-		go func() {
-			defer serverConn.Close()
-			defer listener.Disconnect(conn, "connection lost")
-			for {
-				pk, err := serverConn.ReadPacket()
-				if err != nil {
-					return
-				}
-				if err := conn.WritePacket(pk); err != nil {
-					return
-				}
+			if err := serverConn.WritePacket(pk); err != nil {
+				return
 			}
-		}()
-	}
+		}
+	}()
+	go func() {
+		defer serverConn.Close()
+		defer listener.Disconnect(conn, "connection lost")
+		for {
+			pk, err := serverConn.ReadPacket()
+			if err != nil {
+				return
+			}
+			if err := conn.WritePacket(pk); err != nil {
+				return
+			}
+		}
+	}()
 }
 
 type config struct {
