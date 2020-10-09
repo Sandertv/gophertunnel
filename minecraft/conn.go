5104f5cedc627d1dc4f4e13c61b3df9d643c5f2b
Sandertv <st.ten.veldhuis@gmail.com>
2020-10-09 14:11:05 +0200

    minecraft/conn.go: Return if flushing errors.

--- minecraft/conn.go
+++ minecraft/conn.go
@@ -152,6 +152,7 @@ func newConn(netConn net.Conn, key *ecdsa.PrivateKey, log *log.Logger) *Conn {
 		for range ticker.C {
 			if err := conn.Flush(); err != nil {
 				_ = conn.Close()
+				return
 			}
 		}
 	}()
