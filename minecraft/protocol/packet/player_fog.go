ebc18349b4d28dfdd2fc1756f646f19ff4b254a8
TwistedAsylumMC <twistedasylummc@gmail.com>
2020-11-19 21:55:06 +0000

    Documentation for PlayerFog

--- minecraft/protocol/packet/player_fog.go
+++ minecraft/protocol/packet/player_fog.go
@@ -4,7 +4,11 @@ import (
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
 )
 
+// PlayerFog is sent by the server to render the different fogs in the Stack. The types of fog are controlled
+// by resource packs to change how they are rendered, and the ability to create custom fog.
 type PlayerFog struct {
+	// Stack is a list of fog identifiers to be sent to the client. Examples of fog identifiers are
+	// "minecraft:fog_ocean" and "minecraft:fog_hell".
 	Stack []string
 }
 
