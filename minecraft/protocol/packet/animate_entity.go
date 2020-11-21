a13be66c5c4606c19cd35a06c56263b88dd6b3b0
Sandertv <st.ten.veldhuis@gmail.com>
2020-11-21 15:00:48 +0100

    packet/animate_entity.go: Added documentation.

--- minecraft/protocol/packet/animate_entity.go
+++ minecraft/protocol/packet/animate_entity.go
@@ -4,14 +4,25 @@ import (
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
 )
 
-// AnimateEntity is sent by the server to animate an entity client-side.
+// AnimateEntity is sent by the server to animate an entity client-side. It may be used to play a single
+// animation, or to activate a controller which can start a sequence of animations based on different
+// conditions specified in an animation controller.
+// Much of the documentation of this packet can be found at
+// https://minecraft.gamepedia.com/Bedrock_Edition_beta_animation_documentation.
 type AnimateEntity struct {
-	// TODO: Document all these fields.
-	Animation     string
-	NextState     string
+	// Animation is the name of a single animation to start playing.
+	Animation string
+	// NextState is the first state to start with. These states are declared in animation controllers (which,
+	// in themselves, are animations too). These states in turn may have animations and transitions to move to
+	// a next state.
+	NextState string
+	// StopCondition is a MoLang expression that specifies when the animation should be stopped.
 	StopCondition string
-	Controller    string
-	BlendOutTime  float32
+	// Controller is the animation controller that is used to manage animations. These controllers decide when
+	// to play which animation.
+	Controller string
+	// BlendOutTime does not currently seem to be used.
+	BlendOutTime float32
 	// EntityRuntimeIDs is list of runtime IDs of entities that the animation should be applied to.
 	EntityRuntimeIDs []uint64
 }
