81a6b60a23341dfa4de7b3512ad77d3d53677468
Sandertv <st.ten.veldhuis@gmail.com>
2020-11-21 14:29:14 +0100

    packet/correct_player_move_prediction.go: Added documentation.

--- minecraft/protocol/packet/correct_player_move_prediction.go
+++ minecraft/protocol/packet/correct_player_move_prediction.go
@@ -6,13 +6,19 @@ import (
 )
 
 // CorrectPlayerMovePrediction is sent by the server if and only if StartGame.ServerAuthoritativeMovementMode
-// is set to AuthoritativeMovementModeServerWithRewind.
-// TODO
+// is set to AuthoritativeMovementModeServerWithRewind. The packet is used to correct movement at a specific
+// point in time.
 type CorrectPlayerMovePrediction struct {
+	// Position is the position that the player is supposed to be at at the tick written in the field below.
+	// The client will change its current position based on movement after that tick starting from the
+	// Position.
 	Position mgl32.Vec3
-	Delta    mgl32.Vec3
+	// Delta is the change in position compared to what the client sent as its position at that specific tick.
+	Delta mgl32.Vec3
+	// OnGround specifies if the player was on the ground at the time of the tick below.
 	OnGround bool
-	Tick     uint64
+	// Tick is the tick of the movement which was corrected by this packet.
+	Tick uint64
 }
 
 // ID ...
