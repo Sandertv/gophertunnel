a679bab9482a22463833fd4b9126f22add73a515
Sandertv <st.ten.veldhuis@gmail.com>
2020-11-23 19:32:59 +0100

    packet/add_behaviour_tree.go: Changed documentation.

--- minecraft/protocol/packet/add_behaviour_tree.go
+++ minecraft/protocol/packet/add_behaviour_tree.go
@@ -4,11 +4,10 @@ import (
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
 )
 
-// AddBehaviourTree is sent by the server to the client. Its usage remains unknown, as behaviour packs are
-// typically all sent at the start of the game.
+// AddBehaviourTree is sent by the server to the client. The packet is currently unused by both client and
+// server.
 type AddBehaviourTree struct {
-	// BehaviourTree is a JSON encoded tree containing behaviour. It does not seem like it has any real
-	// effect on the client.
+	// BehaviourTree is an unused string.
 	BehaviourTree string
 }
 
