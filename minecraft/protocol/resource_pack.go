2817148d10c42a4ffb6a62465ea3899d5c217218
TwistedAsylumMC <twistedasylummc@gmail.com>
2020-12-09 14:22:08 +0000

    Split up texture & behaviour packs to add RTXEnabled boolean for texture packs

--- minecraft/protocol/resource_pack.go
+++ minecraft/protocol/resource_pack.go
@@ -1,28 +1,55 @@
 package protocol
 
-// ResourcePackInfo represents a resource pack's info sent over network. It holds information about the
-// resource pack such as its name, description and version.
-type ResourcePackInfo struct {
-	// UUID is the UUID of the resource pack. Each resource pack downloaded must have a different UUID in
+// BehaviourPackInfo represents a behaviour pack's info sent over network. It holds information about the
+// behaviour pack such as its name, description and version.
+type BehaviourPackInfo struct {
+	// UUID is the UUID of the behaviour pack. Each behaviour pack downloaded must have a different UUID in
 	// order for the client to be able to handle them properly.
 	UUID string
-	// Version is the version of the resource pack. The client will cache resource packs sent by the server as
-	// long as they carry the same version. Sending a resource pack with a different version than previously
+	// Version is the version of the behaviour pack. The client will cache behaviour packs sent by the server as
+	// long as they carry the same version. Sending a behaviour pack with a different version than previously
 	// will force the client to re-download it.
 	Version string
-	// Size is the total size in bytes that the resource pack occupies. This is the size of the compressed
-	// archive (zip) of the resource pack.
+	// Size is the total size in bytes that the behaviour pack occupies. This is the size of the compressed
+	// archive (zip) of the behaviour pack.
 	Size uint64
-	// ContentKey is the key used to decrypt the resource pack if it is encrypted. This is generally the case
-	// for marketplace resource packs.
+	// ContentKey is the key used to decrypt the behaviour pack if it is encrypted. This is generally the case
+	// for marketplace behaviour packs.
 	ContentKey string
 	// SubPackName ...
 	SubPackName string
 	// ContentIdentity ...
 	ContentIdentity string
-	// HasScripts specifies if the resource packs has any scripts in it. A client will only download the
-	// resource pack if it supports scripts, which, up to 1.11, only includes Windows 10.
+	// HasScripts specifies if the behaviour packs has any scripts in it. A client will only download the
+	// behaviour pack if it supports scripts, which, up to 1.11, only includes Windows 10.
 	HasScripts bool
+}
+
+// TexturePackInfo represents a texture pack's info sent over network. It holds information about the
+// texture pack such as its name, description and version.
+type TexturePackInfo struct {
+	// UUID is the UUID of the texture pack. Each texture pack downloaded must have a different UUID in
+	// order for the client to be able to handle them properly.
+	UUID string
+	// Version is the version of the texture pack. The client will cache texture packs sent by the server as
+	// long as they carry the same version. Sending a texture pack with a different version than previously
+	// will force the client to re-download it.
+	Version string
+	// Size is the total size in bytes that the texture pack occupies. This is the size of the compressed
+	// archive (zip) of the texture pack.
+	Size uint64
+	// ContentKey is the key used to decrypt the behaviour pack if it is encrypted. This is generally the case
+	// for marketplace texture packs.
+	ContentKey string
+	// SubPackName ...
+	SubPackName string
+	// ContentIdentity ...
+	ContentIdentity string
+	// HasScripts specifies if the texture packs has any scripts in it. A client will only download the
+	// behaviour pack if it supports scripts, which, up to 1.11, only includes Windows 10.
+	HasScripts bool
+	// RTXEnabled ...
+	RTXEnabled bool
 }
 
 // StackResourcePack represents a resource pack sent on the stack of the client. When sent, the client will
@@ -39,8 +66,8 @@ type StackResourcePack struct {
 	SubPackName string
 }
 
-// PackInfo reads/writes a ResourcePackInfo x using IO r.
-func PackInfo(r IO, x *ResourcePackInfo) {
+// BehaviourPackInformation reads/writes a BehaviourPackInfo x using IO r.
+func BehaviourPackInformation(r IO, x *BehaviourPackInfo) {
 	r.String(&x.UUID)
 	r.String(&x.Version)
 	r.Uint64(&x.Size)
@@ -48,6 +75,18 @@ func PackInfo(r IO, x *ResourcePackInfo) {
 	r.String(&x.SubPackName)
 	r.String(&x.ContentIdentity)
 	r.Bool(&x.HasScripts)
+}
+
+// TexturePackInformation reads/writes a TexturePackInfo x using IO r.
+func TexturePackInformation(r IO, x *TexturePackInfo) {
+	r.String(&x.UUID)
+	r.String(&x.Version)
+	r.Uint64(&x.Size)
+	r.String(&x.ContentKey)
+	r.String(&x.SubPackName)
+	r.String(&x.ContentIdentity)
+	r.Bool(&x.HasScripts)
+	r.Bool(&x.RTXEnabled)
 }
 
 // StackPack reads/writes a StackResourcePack x using IO r.
