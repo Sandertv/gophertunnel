94666294c057e84756c8d7fb8bce136ec754ebdd
Sandertv <st.ten.veldhuis@gmail.com>
2021-01-18 14:46:51 +0100

    packet/decoder and encoder.go: Use sync.Pools for pooling compressors, decompressors and buffers.

--- minecraft/protocol/packet/encoder.go
+++ minecraft/protocol/packet/encoder.go
@@ -1,20 +1,19 @@
 package packet
 
 import (
+	"bytes"
 	"crypto/aes"
 	"fmt"
 	"github.com/klauspost/compress/flate"
-	"github.com/sandertv/gophertunnel/internal/dynamic"
 	"io"
+	"io/ioutil"
+	"sync"
 )
 
 // Encoder handles the encoding of Minecraft packets that are sent to an io.Writer. The packets are compressed
 // and optionally encoded before they are sent to the io.Writer.
 type Encoder struct {
-	compressor writeCloseResetter
-	writer     io.Writer
-	buf        *dynamic.Buffer
-
+	w       io.Writer
 	encrypt *encrypt
 }
 
@@ -27,11 +26,8 @@ type writeCloseResetter interface {
 // NewEncoder returns a new Encoder for the io.Writer passed. Each final packet produced by the Encoder is
 // sent with a single call to io.Writer.Write().
 func NewEncoder(w io.Writer) *Encoder {
-	f, _ := flate.NewWriter(w, 6)
 	return &Encoder{
-		writer:     w,
-		compressor: f,
-		buf:        dynamic.NewBuffer(make([]byte, 0, 1024*1024)),
+		w: w,
 	}
 }
 
@@ -45,30 +41,35 @@ func (encoder *Encoder) EnableEncryption(keyBytes [32]byte) {
 // Encode encodes the packets passed. It writes all of them as a single packet which is  compressed and
 // optionally encrypted.
 func (encoder *Encoder) Encode(packets [][]byte) error {
+	buf := bufferPool.Get().(*bytes.Buffer)
 	defer func() {
-		// Reset both buffers so that they can be re-used the next time Encoder encodes packets.
-		encoder.buf.Reset()
+		// Reset the buffer so we can return it to the buffer pool safely.
+		buf.Reset()
+		bufferPool.Put(buf)
 	}()
-	if err := encoder.buf.WriteByte(header); err != nil {
+	if err := buf.WriteByte(header); err != nil {
 		return fmt.Errorf("error writing 0xfe header: %v", err)
 	}
 
-	encoder.compressor.Reset(encoder.buf)
+	w := compressPool.Get().(writeCloseResetter)
+	defer compressPool.Put(w)
+
+	w.Reset(buf)
 	l := make([]byte, 5)
 
 	for _, packet := range packets {
 		// Each packet is prefixed with a varuint32 specifying the length of the packet.
-		if err := writeVaruint32(encoder.compressor, uint32(len(packet)), l); err != nil {
+		if err := writeVaruint32(w, uint32(len(packet)), l); err != nil {
 			return fmt.Errorf("error writing varuint32 length: %v", err)
 		}
-		if _, err := encoder.compressor.Write(packet); err != nil {
+		if _, err := w.Write(packet); err != nil {
 			return fmt.Errorf("error writing packet payload: %v", err)
 		}
 	}
 
 	// We compress the data and write the full data to the io.Writer. The data returned includes the header
 	// we wrote at the start.
-	b, err := encoder.compress()
+	b, err := encoder.compress(w, buf)
 	if err != nil {
 		return err
 	}
@@ -78,7 +79,7 @@ func (encoder *Encoder) Encode(packets [][]byte) error {
 		// compressed data of this packet.
 		b = encoder.encrypt.encrypt(b)
 	}
-	if _, err := encoder.writer.Write(b); err != nil {
+	if _, err := encoder.w.Write(b); err != nil {
 		return fmt.Errorf("error writing compressed packet to io.Writer: %v", err)
 	}
 	return nil
@@ -86,11 +87,11 @@ func (encoder *Encoder) Encode(packets [][]byte) error {
 
 // compress compresses the data passed using the writer passed and returns it in a byte slice. It returns
 // the full content of encoder.buf, so any data currently set in that buffer will also be returned.
-func (encoder *Encoder) compress() ([]byte, error) {
-	if err := encoder.compressor.Close(); err != nil {
+func (encoder *Encoder) compress(w writeCloseResetter, buf *bytes.Buffer) ([]byte, error) {
+	if err := w.Close(); err != nil {
 		return nil, fmt.Errorf("error closing compressor: %v", err)
 	}
-	return encoder.buf.Bytes(), nil
+	return buf.Bytes(), nil
 }
 
 // writeVaruint32 writes a uint32 to the destination buffer passed with a size of 1-5 bytes. It uses byte
@@ -111,4 +112,19 @@ func writeVaruint32(dst writeCloseResetter, x uint32, b []byte) error {
 	b[i] = byte(x)
 	_, err := dst.Write(b[:i+1])
 	return err
+}
+
+// compressPool is a sync.Pool for writeCloseResetter flate readers. These are pooled for connections.
+var compressPool = sync.Pool{
+	New: func() interface{} {
+		w, _ := flate.NewWriter(ioutil.Discard, 6)
+		return w
+	},
+}
+
+// bufferPool is a sync.Pool for buffers used to write compressed data to.
+var bufferPool = sync.Pool{
+	New: func() interface{} {
+		return bytes.NewBuffer(make([]byte, 0, 256))
+	},
 }
