05cbb8ba9378be128b1d5200ed71f875d80b28d4
Prim69 <almusawihashim@gmail.com>
2021-03-23 16:33:00 -0400

    oops

--- minecraft/protocol/login/data.go
+++ minecraft/protocol/login/data.go
@@ -138,11 +138,11 @@ type ClientData struct {
 	// SkinID is a unique ID produced for the skin, for example 'c18e65aa-7b21-4637-9b63-8ad63622ef01_Alex'
 	// for the default Alex skin.
 	SkinID string `json:"SkinId"`
-	// SkinImageHeight and SkinImageWidth are the dimensions of the skin's image data.
-	PlayFabID string `json:"PlayFabId"`
 	// PlayFabID is the PlayFab ID produced for the player's skin. PlayFab is the company that hosts the
 	// Marketplace, skins and other related features from the game. This ID is the ID of the skin used to
 	// store the skin inside of PlayFab.
+	PlayFabID string `json:"PlayFabId"`
+	// SkinImageHeight and SkinImageWidth are the dimensions of the skin's image data.
 	SkinImageHeight, SkinImageWidth int
 	// SkinResourcePatch is a base64 encoded string which holds JSON data. The content of the JSON data points
 	// to the assets that should be used to shape the skin. An example with a head animation can be found
