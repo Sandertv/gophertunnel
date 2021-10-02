03dd91672ed1ad7231791393eb1b3b5b553f2b1f
Jviguy <62412449+Jviguy@users.noreply.github.com>
2021-10-02 01:48:11 -0400

    add usage cases for all requestTypes

--- minecraft/protocol/packet/npc_request.go
+++ minecraft/protocol/packet/npc_request.go
@@ -3,12 +3,25 @@ package packet
 import "github.com/sandertv/gophertunnel/minecraft/protocol"
 
 const (
+	// CommandString is used to store a json array of json objects representing buttons.
 	NPCRequestActionSetActions = iota
+	// ActionType is used to store the index of the button clicked
+	// The behaviour of handling this can be different so that we either just handle the action now
+	// Or store it and execute it when the ExecuteClosingCommands is called.
 	NPCRequestActionExecuteAction
+	// ExecuteClosingCommands is used for executing commands (vanilla) or a callback based on when the actual window is closed or when the buttons type is closed.
 	NPCRequestActionExecuteClosingCommands
+	// SetName sets the name of the given NPC
+	// The name is stored in the CommandString and should be used to set the nameTag of the given entity
 	NPCRequestActionSetName
+	// SetSkin sets the current skin of the NPC
+	// This works off of skin index's usually in packs, but as far as I know this doesn't work on servers.
+	// The skin index is stored in the CommandString field
 	NPCRequestActionSetSkin
+	// SetInteractText sets the content (in the nbt data of the entity) to a string
+	// This new text is stored in the CommandString field
 	NPCRequestActionSetInteractText
+	// ExecuteOpeningCommands executes a command thats type is set to open or is forced to by the server!
 	NPCRequestActionExecuteOpeningCommands
 )
 
@@ -24,10 +37,12 @@ type NPCRequest struct {
 	RequestType byte
 	// CommandString is the command string set in the NPC. It may consist of multiple commands, depending on
 	// what the player set in it.
+	// Note this depends on the window as SetActions uses this as a json array of buttons.
 	CommandString string
-	// ActionType is the type of the action to execute.
+	// ActionType is the index of the given button selected
+	// Note when this is called we shouldn't run given buttons code as a dialogue isn't submitted until ExecuteClosingCommands is called. 
 	ActionType byte
-	// SceneName is the name of the scene.
+	// SceneName is the name of the scene this is usually just "".
 	SceneName string
 }
 
