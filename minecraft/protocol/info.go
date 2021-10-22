9f3160665c56fe983dcd1f915d37bec5755f0598
JustTal <justtal@protonmail.com>
2021-10-21 22:12:58 -0500

    Protocol update to support v1.17.40

--- minecraft/protocol/info.go
+++ minecraft/protocol/info.go
@@ -2,7 +2,7 @@ package protocol
 
 const (
 	// CurrentProtocol is the current protocol version for the version below.
-	CurrentProtocol = 465
+	CurrentProtocol = 471
 	// CurrentVersion is the current version of Minecraft as supported by the `packet` package.
-	CurrentVersion = "1.17.30"
+	CurrentVersion = "1.17.40"
 )
---
+++ minecraft/protocol/packet/sub_chunk.go
@@ -0,0 +1,66 @@
+package packet
+
+import (
+	"github.com/sandertv/gophertunnel/minecraft/protocol"
+)
+
+const (
+	SubChunkRequestResultUndefined int32 = iota
+	SubChunkRequestResultSuccess
+	SubChunkRequestResultChunkNotFound
+	SubChunkRequestResultInvalidDimension
+	SubChunkRequestResultPlayerNotFound
+	SubChunkRequestResultIndexOutOfBounds
+)
+
+const (
+	HeightMapDataTypeNone byte = iota
+	HeightMapDataTypeHasData
+	HeightMapDataTypeTooHigh
+	HeightMapDataTypeTooLow
+)
+
+// SubChunk sends sub chunk data about a specific chunk using its position and dimension.
+type SubChunk struct {
+	// Dimension is the dimension of the sub chunk.
+	Dimension int32
+	// SubChunkX, SubChunkY, and SubChunkZ help identify the sub chunk.
+	SubChunkX, SubChunkY, SubChunkZ int32
+	// Data is the actual sub chunk data, such as the blocks.
+	Data []byte
+	// RequestResult is one of the values above.
+	RequestResult int32
+	// HeightMapType is one of the values above.
+	HeightMapType byte
+	// HeightMapData is the data for the height map.
+	HeightMapData []byte
+}
+
+// ID ...
+func (*SubChunk) ID() uint32 {
+	return IDSubChunk
+}
+
+// Marshal ...
+func (pk *SubChunk) Marshal(w *protocol.Writer) {
+	w.Varint32(&pk.Dimension)
+	w.Varint32(&pk.SubChunkX)
+	w.Varint32(&pk.SubChunkY)
+	w.Varint32(&pk.SubChunkZ)
+	w.ByteSlice(&pk.Data)
+	w.Varint32(&pk.RequestResult)
+	w.Uint8(&pk.HeightMapType)
+	w.Bytes(&pk.HeightMapData)
+}
+
+// Unmarshal ...
+func (pk *SubChunk) Unmarshal(r *protocol.Reader) {
+	r.Varint32(&pk.Dimension)
+	r.Varint32(&pk.SubChunkX)
+	r.Varint32(&pk.SubChunkY)
+	r.Varint32(&pk.SubChunkZ)
+	r.ByteSlice(&pk.Data)
+	r.Varint32(&pk.RequestResult)
+	r.Uint8(&pk.HeightMapType)
+	r.Bytes(&pk.HeightMapData)
+}
