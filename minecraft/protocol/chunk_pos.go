4a792f4db50298c9735481141f3f37479bc1c95e
JustTal <justtal@protonmail.com>
2022-02-10 18:57:47 -0600

    protocol/chunk_pos.go: Fix docs.

--- minecraft/protocol/chunk_pos.go
+++ minecraft/protocol/chunk_pos.go
@@ -1,7 +1,6 @@
 package protocol
 
-// ChunkPos is the position of a chunk. It is composed of two integers, and is typically written as either
-// two varint32s.
+// ChunkPos is the position of a chunk. It is composed of two integers and is written as two varint32s.
 type ChunkPos [2]int32
 
 // X returns the X coordinate of the chunk position. It is equivalent to ChunkPos[0].
@@ -14,21 +13,21 @@ func (pos ChunkPos) Z() int32 {
 	return pos[1]
 }
 
-// SubChunkPos is the position of a subchunk. The X and Z coordinates are the coordinates of the chunk, and the Y
-// coordinate is the absolute subchunk index.
+// SubChunkPos is the position of a sub-chunk. The X and Z coordinates are the coordinates of the chunk, and the Y
+// coordinate is the absolute sub-chunk index.
 type SubChunkPos [3]int32
 
-// X returns the X coordinate of the subchunk position. It is equivalent to SubChunkPos[0].
+// X returns the X coordinate of the sub-chunk position. It is equivalent to SubChunkPos[0].
 func (pos SubChunkPos) X() int32 {
 	return pos[0]
 }
 
-// Y returns the Y coordinate of the subchunk position. It is equivalent to SubChunkPos[1].
+// Y returns the Y coordinate of the sub-chunk position. It is equivalent to SubChunkPos[1].
 func (pos SubChunkPos) Y() int32 {
 	return pos[1]
 }
 
-// Z returns the Z coordinate of the subchunk position. It is equivalent to SubChunkPos[2].
+// Z returns the Z coordinate of the sub-chunk position. It is equivalent to SubChunkPos[2].
 func (pos SubChunkPos) Z() int32 {
 	return pos[2]
 }
