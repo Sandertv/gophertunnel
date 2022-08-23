e7c046496f07cb8c9b231177235eb78ec5a2ef5c
olebeck <31539311+olebeck@users.noreply.github.com>
2022-08-23 17:09:03 +0200

    fix code style issues

--- minecraft/realms/realms.go
+++ minecraft/realms/realms.go
@@ -12,21 +12,21 @@ import (
 	"golang.org/x/oauth2"
 )
 
-// RealmsApi is an instance of the realms api with a token
-type RealmsApi struct {
-	token_src  oauth2.TokenSource
-	xbox_token *auth.XBLToken
+// Client is an instance of the realms api with a token.
+type Client struct {
+	tokenSrc oauth2.TokenSource
+	xblToken *auth.XBLToken
 }
 
-// NewRealmsApi returns a new RealmsApi instance with the supplied token source for authentication
-func NewRealmsApi(src oauth2.TokenSource) *RealmsApi {
-	return &RealmsApi{
-		token_src: src,
+// NewClient returns a new RealmsApi instance with the supplied token source for authentication.
+func NewClient(src oauth2.TokenSource) *Client {
+	return &Client{
+		tokenSrc: src,
 	}
 }
 
-// RealmPlayer in a realm returned from the api
-type RealmPlayer struct {
+// Player is a player in a Realm.
+type Player struct {
 	UUID       string `json:"uuid"`
 	Name       string `json:"Name"`
 	Operator   bool   `json:"operator"`
@@ -35,82 +35,81 @@ type RealmPlayer struct {
 	Permission string `json:"permission"`
 }
 
-// Realm returned from the api
+// Realm is the realm structure returned from the api.
 type Realm struct {
-	// the unique id for this realm.
-	Id int `json:"id"`
-	// The subscription ID of the realm.
+	// ID is the unique id for this realm.
+	ID int `json:"id"`
+	// RemoteSubscriptionId is The subscription ID of the realm.
 	RemoteSubscriptionId string `json:"remoteSubscriptionId"`
-	// This is always an empty string.
+	// Owner is always an empty string.
 	Owner string `json:"owner"`
-	// The XboxUserID (XUID) of the owner.
+	// OwnerUUID is the XboxUserID (XUID) of the owner.
 	OwnerUUID string `json:"ownerUUID"`
-	// The name of the Realm.
+	// Name is the name of the Realm.
 	Name string `json:"name"`
-	// This is always an empty string.
-	Motd string `json:"motd"`
-	// The default permission level of the Realm world.
+	// MOTD is always an empty string.
+	MOTD string `json:"motd"`
+	// DefaultPermission is the default permission level of the Realm world.
 	// one of ["MEMBER", "OPERATOR"]
 	DefaultPermission string `json:"defaultPermission"`
-	// The current state of the realm
+	// State is the current state of the realm
 	// one of: ["OPEN", "CLOSED"]
 	State string `json:"state"`
-	//  The days remaining before renewal of the Realm as an integer.
+	// DaysLeft is the days remaining before renewal of the Realm as an integer.
 	// (always 0 for Realms where the current user is not the owner)
 	DaysLeft int `json:"daysLeft"`
-	// whether the Realm has expired as a trial or not.
+	// Expired is whether the Realm has expired as a trial or not.
 	Expired bool `json:"expired"`
-	// whether the Realm has expired as a trial or not.
+	// ExpiredTrial is whether the Realm has expired as a trial or not.
 	ExpiredTrial bool `json:"expiredTrial"`
-	// whether the Realm is in its grace period after expiry or not.
+	// GracePeriod is whether the Realm is in its grace period after expiry or not.
 	GracePeriod bool `json:"gracePeriod"`
-	// The world type of the currently loaded world
-	// one of: ["NORMAL", "FLAT"?]
+	// WorldType is the world type of the currently loaded world.
 	WorldType string `json:"worldType"`
 	// Players is a list of the players currently online in the realm
-	// NOTE: this is only sent when directly requesting a realm
-	Players []RealmPlayer `json:"players"`
-	// MaxPlayers how many player slots this realm has
+	// NOTE: this is only sent when directly requesting a realm.
+	Players []Player `json:"players"`
+	// MaxPlayers is how many player slots this realm has.
 	MaxPlayers int `json:"maxPlayers"`
-	// always null
+	// MinigameName is always null
 	MinigameName string `json:"minigameName"`
-	// always null
-	MinigameId string `json:"minigameId"`
-	// always null
+	// MinigameID is always null
+	MinigameID string `json:"minigameId"`
+	// MinigameImage is always null
 	MinigameImage string `json:"minigameImage"`
-	// unused, always 1
+	// ActiveSlot is unused, always 1
 	ActiveSlot int `json:"activeSlot"`
-	// unused, always null
+	// Slots is unused, always null
 	Slots []struct{} `json:"slots"`
-	// Unknown, always false. (even when member or owner)
+	// Member is Unknown, always false. (even when member or owner)
 	Member bool `json:"member"`
-	// The ID of the associated Xbox Live club as an integer.
-	ClubId int `json:"clubId"`
-	// Unknown, always null
+	// ClubID is the ID of the associated Xbox Live club as an integer.
+	ClubID int `json:"clubId"`
+	// SubscriptionRefreshStatus is Unknown, always null.
 	SubscriptionRefreshStatus struct{} `json:"subscriptionRefreshStatus"`
 
-	// instance of RealmsApi that this belongs to
-	_realmsApi *RealmsApi
+	// client is the instance of Client that this belongs to.
+	client *Client
 }
 
-// GetAddress requests the address and port to connect to this realm from the api
-// if wait is true it will poll the api until the server is started
-// else it will just return the http error
-func (r *Realm) GetAddress(ctx context.Context, wait bool) (address string, err error) {
+// Address requests the address and port to connect to this realm from the api,
+// will wait for the realm to start if it is currently offline.
+func (r *Realm) Address(ctx context.Context) (address string, err error) {
 	ticker := time.NewTicker(time.Second * 3)
 	defer ticker.Stop()
-	i := 0
+	firstRequest := true
 	for range ticker.C {
-		i++
-		body, status, err := r._realmsApi.getRequest(ctx, fmt.Sprintf("/worlds/%d/join", r.Id))
+		body, status, err := r.client.request(ctx, fmt.Sprintf("/worlds/%d/join", r.ID))
 		if err != nil {
-			if status == 503 && wait {
-				fmt.Printf("Waiting for the realm to start... %d\033[K\r", i)
+			if status == 503 && ctx.Err() == nil {
+				if firstRequest {
+					fmt.Println("Waiting for Realm to start")
+				}
+				firstRequest = false
 				continue
 			}
 			return "", err
 		}
-		println()
 
 		var data struct {
 			Address       string `json:"address"`
@@ -124,10 +123,10 @@ func (r *Realm) GetAddress(ctx context.Context, wait bool) (address string, err 
 	panic("unreachable")
 }
 
-// GetPlayers gets all the players currently on this realm
+// PlayerList gets all the players currently on this realm,
 // Returns a 403 error if the current user is not the owner of the Realm.
-func (r *Realm) GetPlayers(ctx context.Context) (players []RealmPlayer, err error) {
-	body, _, err := r._realmsApi.getRequest(ctx, fmt.Sprintf("/worlds/%d", r.Id))
+func (r *Realm) PlayerList(ctx context.Context) (players []Player, err error) {
+	body, _, err := r.client.request(ctx, fmt.Sprintf("/worlds/%d", r.ID))
 	if err != nil {
 		return nil, err
 	}
@@ -140,23 +139,23 @@ func (r *Realm) GetPlayers(ctx context.Context) (players []RealmPlayer, err erro
 	return response.Players, nil
 }
 
-// getXboxToken returns the xbox token used for the api
-func (r *RealmsApi) getXboxToken(ctx context.Context) (*auth.XBLToken, error) {
-	if r.xbox_token != nil {
-		return r.xbox_token, nil
+// xboxToken returns the xbox token used for the api.
+func (r *Client) xboxToken(ctx context.Context) (*auth.XBLToken, error) {
+	if r.xblToken != nil {
+		return r.xblToken, nil
 	}
 
-	t, err := r.token_src.Token()
+	t, err := r.tokenSrc.Token()
 	if err != nil {
 		return nil, err
 	}
 
-	r.xbox_token, err = auth.RequestXBLToken(ctx, t, "https://pocket.realms.minecraft.net/")
-	return r.xbox_token, err
+	r.xblToken, err = auth.RequestXBLToken(ctx, t, "https://pocket.realms.minecraft.net/")
+	return r.xblToken, err
 }
 
-// getRequest sends a get request to path with the right headers for the api set
-func (r *RealmsApi) getRequest(ctx context.Context, path string) (body []byte, status int, err error) {
+// request sends an http get request to path with the right headers for the api set.
+func (r *Client) request(ctx context.Context, path string) (body []byte, status int, err error) {
 	if string(path[0]) != "/" {
 		path = "/" + path
 	}
@@ -166,7 +165,7 @@ func (r *RealmsApi) getRequest(ctx context.Context, path string) (body []byte, s
 	}
 	req.Header.Set("User-Agent", "MCPE/UWP")
 	req.Header.Set("Client-Version", "1.10.1")
-	xbl, err := r.getXboxToken(ctx)
+	xbl, err := r.xboxToken(ctx)
 	if err != nil {
 		return nil, 0, err
 	}
@@ -190,9 +189,9 @@ func (r *RealmsApi) getRequest(ctx context.Context, path string) (body []byte, s
 	return body, resp.StatusCode, nil
 }
 
-// GetRealms gets a list of all realms the token has access to
-func (r *RealmsApi) GetRealms(ctx context.Context) ([]Realm, error) {
-	body, _, err := r.getRequest(ctx, "/worlds")
+// Realms gets a list of all realms the token has access to.
+func (c *Client) Realms(ctx context.Context) ([]Realm, error) {
+	body, _, err := c.request(ctx, "/worlds")
 	if err != nil {
 		return nil, err
 	}
@@ -206,7 +205,7 @@ func (r *RealmsApi) GetRealms(ctx context.Context) ([]Realm, error) {
 
 	realms := response.Servers
 	for i := range realms {
-		realms[i]._realmsApi = r
+		realms[i].client = c
 	}
 
 	return realms, nil
