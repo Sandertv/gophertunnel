9cd772a168df71418e468ca3510aae9f0199308e
Hashim <13991048+Prim69@users.noreply.github.com>
2023-02-17 22:05:49 -0500

    Update dial.go

--- minecraft/dial.go
+++ minecraft/dial.go
@@ -56,9 +56,9 @@ type Dialer struct {
 
 	// DownloadResourcePack is called individually for every texture and behaviour pack sent by the connection when
 	// using Dialer.Dial(), and can be used to stop the pack from being downloaded. The function is called with the UUID
-	// and version of the resource pack, and the total amount of packs. The boolean returned determines if the pack will
-	// be downloaded or not.
-	DownloadResourcePack func(id uuid.UUID, version string, totalPacks int) bool
+	// and version of the resource pack, the numer of the current pack being downloaded, and the total amount of packs. 
+	// The boolean returned determines if the pack will be downloaded or not.
+	DownloadResourcePack func(id uuid.UUID, version string, currentPack, totalPacks int) bool
 
 	// Protocol is the Protocol version used to communicate with the target server. By default, this field is
 	// set to the current protocol as implemented in the minecraft/protocol package. Note that packets written
