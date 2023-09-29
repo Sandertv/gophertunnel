1d77d1adb6a273ba55d52629616265131b35d741
Javier León <58715544+JavierLeon9966@users.noreply.github.com>
2023-09-29 16:02:24 -0300

    Allow passing empty time.Time in SetReadDeadline()

--- minecraft/conn.go
+++ minecraft/conn.go
@@ -487,12 +487,11 @@ func (conn *Conn) SetDeadline(t time.Time) error {
 // SetReadDeadline sets the read deadline of the Conn to the time passed. The time must be after time.Now().
 // Passing an empty time.Time to the method (time.Time{}) results in the read deadline being cleared.
 func (conn *Conn) SetReadDeadline(t time.Time) error {
-	if t.Before(time.Now()) {
-		panic(fmt.Errorf("error setting read deadline: time passed is before time.Now()"))
-	}
 	empty := time.Time{}
 	if t == empty {
 		conn.readDeadline = make(chan time.Time)
+	} else if t.Before(time.Now()) {
+		panic(fmt.Errorf("error setting read deadline: time passed is before time.Now()"))
 	} else {
 		conn.readDeadline = time.After(time.Until(t))
 	}
