e1afbf2dccf3c6b6a3521b2923a8e59f0460ae9a
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2024-08-04 12:46:19 -0700

    Update player_auth_input.go
    FIX varint overflows integer when in a boat and breaking a block

--- minecraft/protocol/packet/player_auth_input.go
+++ minecraft/protocol/packet/player_auth_input.go
@@ -161,14 +161,14 @@ func (pk *PlayerAuthInput) Marshal(io protocol.IO) {
 	if pk.InputData&InputFlagPerformItemStackRequest != 0 {
 		protocol.Single(io, &pk.ItemStackRequest)
 	}
-
+	
+	if pk.InputData&InputFlagPerformBlockActions != 0 {
+		protocol.SliceVarint32Length(io, &pk.BlockActions)
+	}
+	
 	if pk.InputData&InputFlagClientPredictedVehicle != 0 {
 		io.Vec2(&pk.VehicleRotation)
 		io.Varint64(&pk.ClientPredictedVehicle)
-	}
-
-	if pk.InputData&InputFlagPerformBlockActions != 0 {
-		protocol.SliceVarint32Length(io, &pk.BlockActions)
 	}
 
 	io.Vec2(&pk.AnalogueMoveVector)
