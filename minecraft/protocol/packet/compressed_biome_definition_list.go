0b4735cc2f853bdf6f538a359d72df98159e4ace
Hashim <hashimssoftware@gmail.com>
2024-08-04 14:06:01 -0400

    fix incorrect packet

--- minecraft/protocol/packet/compressed_biome_definition_list.go
+++ minecraft/protocol/packet/compressed_biome_definition_list.go
@@ -9,7 +9,7 @@ import (
 type CompressedBiomeDefinitionList struct {
 	// Biomes is a map of biomes with their identifier as key, and the biome data as value. The biome data contains many
 	// different fields such as climate, surface materials and generation rules etc.
-	Biomes map[string]any
+	SerialisedBiomeDefinitions []byte
 }
 
 // ID ...
@@ -18,5 +18,5 @@ func (*CompressedBiomeDefinitionList) ID() uint32 {
 }
 
 func (pk *CompressedBiomeDefinitionList) Marshal(io protocol.IO) {
-	io.CompressedBiomeDefinitions(&pk.Biomes)
+	io.Bytes(&pk.SerialisedBiomeDefinitions)
 }
