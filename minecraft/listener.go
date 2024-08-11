0cdb9b18b5e97ea642b36f7532475fd62349b588
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2024-08-11 13:39:05 -0700

    Fix the default compression

--- minecraft/listener.go
+++ minecraft/listener.go
@@ -54,7 +54,7 @@ type ListenConfig struct {
 	// be disconnected.
 	AcceptedProtocols []Protocol
 	// Compression is the packet.Compression to use for packets sent over this Conn. If set to nil, the compression
-	// will default to packet.flateCompression.
+	// will default to packet.snappyCompression.
 	Compression packet.Compression
 	// FlushRate is the rate at which packets sent are flushed. Packets are buffered for a duration up to
 	// FlushRate and are compressed/encrypted together to improve compression ratios. The lower this
