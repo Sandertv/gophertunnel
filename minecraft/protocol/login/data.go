ae51485a6bbfd3a9e04bd829656ed518ee8fe931
FDUTCH <para.trabajos9999@gmail.com>
2025-01-16 19:44:42 +0100

    fix comments

--- minecraft/protocol/login/data.go
+++ minecraft/protocol/login/data.go
@@ -34,12 +34,12 @@ type IdentityData struct {
 	TitleID string `json:"titleId,omitempty"`
 }
 
-// checkOfflineUsername is used to check if a username is valid according to the Microsoft specification: "You can
-// use up to 15 characters: Aa-Zz, 0-9, and single spaces. It cannot start with a number and cannot start or
-// end with a space."
+// checkOfflineUsername is used to check if a username is valid for normal Minecraft client.
 var checkOfflineUsername = regexp.MustCompile("[ \\p{L}]").MatchString
 
-// checkOnlineUsername is used to check if a username is valid for normal Minecraft client.
+// checkOnlineUsername is used to check if a username is valid according to the Microsoft specification: "You can
+// // use up to 15 characters: Aa-Zz, 0-9, and single spaces. It cannot start with a number and cannot start or
+// // end with a space."
 var checkOnlineUsername = regexp.MustCompile("[A-Za-z0-9 ]").MatchString
 
 // Validate validates the identity data. It returns an error if any data contained in the IdentityData is
