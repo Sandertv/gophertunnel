72958edf92496b96c36342deb8607219c9d2747a
Fadhel D. <cooldogedev@gmail.com>
2025-06-18 13:12:52 +0300

    protocol/login: Fixed authentication issues (#324)

--- minecraft/dial.go
+++ minecraft/dial.go
@@ -220,7 +220,7 @@ func (d Dialer) DialContext(ctx context.Context, network, address string) (conn 
 		if !d.KeepXBLIdentityData {
 			clearXBLIdentityData(&conn.identityData)
 		}
-		request = login.EncodeOffline(conn.identityData, conn.clientData, key)
+		request = login.EncodeOffline(conn.identityData, conn.clientData, key, d.EnableLegacyAuth)
 	} else {
 		// We login as an Android device and this will show up in the 'titleId' field in the JWT chain, which
 		// we can't edit. We just enforce Android data for logging in.
