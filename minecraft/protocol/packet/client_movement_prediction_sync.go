109c459ca0356464becf9ae04d94916781a6b0f5
didntpot <didntpot@protonmail.com>
2025-06-18 15:39:30 +0100

    packet/client_movement_prediction_sync.go: Use EntityDataFlagCount

--- minecraft/protocol/packet/client_movement_prediction_sync.go
+++ minecraft/protocol/packet/client_movement_prediction_sync.go
@@ -4,8 +4,6 @@ import (
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
 )
 
-const ClientMovementPredictionSyncBitsetSize = 125
-
 // ClientMovementPredictionSync is sent by the client to the server periodically if the client has received
 // movement corrections from the server, containing information about client-predictions that are relevant
 // to movement.
@@ -43,7 +41,7 @@ func (*ClientMovementPredictionSync) ID() uint32 {
 }
 
 func (pk *ClientMovementPredictionSync) Marshal(io protocol.IO) {
-	io.Bitset(&pk.ActorFlags, ClientMovementPredictionSyncBitsetSize)
+	io.Bitset(&pk.ActorFlags, protocol.EntityDataFlagCount)
 	io.Float32(&pk.BoundingBoxScale)
 	io.Float32(&pk.BoundingBoxWidth)
 	io.Float32(&pk.BoundingBoxHeight)
