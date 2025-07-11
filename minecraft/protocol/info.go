67d3dc9b404e20ad02e0136f93ecc76a7b64cc80
Fadhel D. <cooldogedev@gmail.com>
2025-07-12 02:59:02 +0300

    minecraft/protocol: Support 1.21.100

--- minecraft/protocol/info.go
+++ minecraft/protocol/info.go
@@ -2,7 +2,7 @@ package protocol
 
 const (
 	// CurrentProtocol is the current protocol version for the version below.
-	CurrentProtocol = 819
+	CurrentProtocol = 827
 	// CurrentVersion is the current version of Minecraft as supported by the `packet` package.
-	CurrentVersion = "1.21.93"
+	CurrentVersion = "1.21.100"
 )
