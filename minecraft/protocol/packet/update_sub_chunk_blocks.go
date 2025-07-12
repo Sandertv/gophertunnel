91386cee3a3cc031ecf23ed9b5b034ec1a333dbe
Happy2018new <chenbo79800@163.com>
2025-07-12 19:08:43 +0800

    minecraft/protocol/packet/update_sub_chunk_blocks.go: Fix data type of `Position`

--- minecraft/protocol/packet/update_sub_chunk_blocks.go
+++ minecraft/protocol/packet/update_sub_chunk_blocks.go
@@ -4,8 +4,9 @@ import "github.com/sandertv/gophertunnel/minecraft/protocol"
 
 // UpdateSubChunkBlocks is essentially just UpdateBlock packet, however for a set of blocks in a sub-chunk.
 type UpdateSubChunkBlocks struct {
-	// Position is the position of the sub-chunk being referred to.
-	Position protocol.SubChunkPos
+	// Position is the block position of the sub-chunk being referred to.
+	// If Position is (x, y, z), then (x>>4, y>>4, z>>4) is the corresponding sub chunk position.
+	Position protocol.BlockPos
 	// Blocks contains each updated block change entry.
 	Blocks []protocol.BlockChangeEntry
 	// Extra contains each updated block change entry for the second layer, usually for waterlogged blocks.
@@ -18,7 +19,7 @@ func (*UpdateSubChunkBlocks) ID() uint32 {
 }
 
 func (pk *UpdateSubChunkBlocks) Marshal(io protocol.IO) {
-	io.SubChunkPos(&pk.Position)
+	io.UBlockPos(&pk.Position)
 	protocol.Slice(io, &pk.Blocks)
 	protocol.Slice(io, &pk.Extra)
 }
