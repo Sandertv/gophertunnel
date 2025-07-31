9e3fd1c825a700ee2960c701c7cd4f2a9010243b
Fadhel D. <cooldogedev@gmail.com>
2025-07-31 12:36:10 +0300

    packet/start_game.go: correct the marshalling order for `TickDeathSystemsEnabled`

--- minecraft/protocol/packet/start_game.go
+++ minecraft/protocol/packet/start_game.go
@@ -251,10 +251,10 @@ type StartGame struct {
 	// its index in the expected block palette. This is useful for servers that wish to support multiple protocol versions
 	// and custom blocks, but it will result in extra bytes being written for every block in a sub chunk palette.
 	UseBlockNetworkIDHashes bool
-	// ServerAuthoritativeSound is currently unknown as to what it does.
-	ServerAuthoritativeSound bool
 	// TickDeathSystemsEnabled specifies if the new tick death systems are enabled.
 	TickDeathSystemsEnabled bool
+	// ServerAuthoritativeSound is currently unknown as to what it does.
+	ServerAuthoritativeSound bool
 }
 
 // ID ...
@@ -340,6 +340,6 @@ func (pk *StartGame) Marshal(io protocol.IO) {
 	io.UUID(&pk.WorldTemplateID)
 	io.Bool(&pk.ClientSideGeneration)
 	io.Bool(&pk.UseBlockNetworkIDHashes)
-	io.Bool(&pk.ServerAuthoritativeSound)
 	io.Bool(&pk.TickDeathSystemsEnabled)
+	io.Bool(&pk.ServerAuthoritativeSound)
 }
