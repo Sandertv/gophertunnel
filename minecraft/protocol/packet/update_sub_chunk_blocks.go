450532971415bfa9b91307ac0e1d35a0f47c91a8
Happy2018new <chenbo79800@163.com>
2025-09-02 09:35:18 +0800

    minecraft/protocol/packet/update_sub_chunk_blocks.go: Remove unnecessary line

--- minecraft/protocol/packet/update_sub_chunk_blocks.go
+++ minecraft/protocol/packet/update_sub_chunk_blocks.go
@@ -5,7 +5,6 @@ import "github.com/sandertv/gophertunnel/minecraft/protocol"
 // UpdateSubChunkBlocks is essentially just UpdateBlock packet, however for a set of blocks in a sub-chunk.
 type UpdateSubChunkBlocks struct {
 	// Position is the block position of the sub-chunk being referred to.
-	// If Position is (x, y, z), then (x>>4, y>>4, z>>4) is the corresponding sub chunk position.
 	Position protocol.BlockPos
 	// Blocks contains each updated block change entry.
 	Blocks []protocol.BlockChangeEntry
