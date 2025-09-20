92cd405a34dd6f60dbe14be49d6853a42fec45b7
ismail <dev.1smail.eke34@gmail.com>
2025-09-20 17:03:52 +0300

    LegacyTelemetryEventPacket: wrong field name

--- minecraft/protocol/packet/event.go
+++ minecraft/protocol/packet/event.go
@@ -7,9 +7,9 @@ import (
 // Event is sent by the server to send an event with additional data. It is typically sent to the client for
 // telemetry reasons, much like the SimpleEvent packet.
 type Event struct {
-	// EntityRuntimeID is the runtime ID of the player. The runtime ID is unique for each world session, and
-	// entities are generally identified in packets using this runtime ID.
-	EntityRuntimeID int64
+	// EntityUniqueID is the unique ID of the player. The unique ID is unique for each world session, and
+	// entities are generally identified in packets using this unique ID.
+	EntityUniqueID int64
 	// UsePlayerID ...
 	// TODO: Figure out what UsePlayerID is for.
 	UsePlayerID byte
@@ -23,7 +23,7 @@ func (*Event) ID() uint32 {
 }
 
 func (pk *Event) Marshal(io protocol.IO) {
-	io.Varint64(&pk.EntityRuntimeID)
+	io.Varint64(&pk.EntityUniqueID)
 	io.EventType(&pk.Event)
 	io.Uint8(&pk.UsePlayerID)
 	pk.Event.Marshal(io)
