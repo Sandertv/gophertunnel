e1093115923a8c1cdf254d0f2848f95e564c49fb
CJ <CJMustard21@gmail.com>
2025-10-28 22:03:21 -0600

    types

--- minecraft/protocol/packet/graphics_override_parameter.go
+++ minecraft/protocol/packet/graphics_override_parameter.go
@@ -11,7 +11,7 @@ type GraphicsOverrideParameter struct {
 	// BiomeIdentifier is the identifier of the biome for which the parameters apply.
 	BiomeIdentifier string
 	// ParameterType is the type of parameter being overridden.
-	ParameterType protocol.GraphicsOverrideParameterType
+	ParameterType uint8
 	// Reset indicates whether to reset the parameters.
 	Reset bool
 }
@@ -24,6 +24,6 @@ func (*GraphicsOverrideParameter) ID() uint32 {
 func (pk *GraphicsOverrideParameter) Marshal(io protocol.IO) {
 	protocol.Slice(io, &pk.Values)
 	io.String(&pk.BiomeIdentifier)
-	io.Uint8((*uint8)(&pk.ParameterType))
+	io.Uint8(&pk.ParameterType)
 	io.Bool(&pk.Reset)
 }
