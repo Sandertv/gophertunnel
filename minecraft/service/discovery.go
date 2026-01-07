52fdbb69adf9303f056fe3f723fa9dd52e53bc6a
lactyy <92302002+lactyy@users.noreply.github.com>
2026-01-08 03:50:08 +0900

    minecraft/service/discovery.go: Reorder definitions for clarity

--- minecraft/service/discovery.go
+++ minecraft/service/discovery.go
@@ -10,78 +10,14 @@ import (
 	"github.com/sandertv/gophertunnel/minecraft/service/internal"
 )
 
-// Discover obtains a Discover for the specific version for the specific
-// application type. The returned Discovery contains environments for
-// various services in Minecraft: Bedrock Edition and will be used as
-// the configuration for each service.
-//
-// Discover caches the result and can be called multiple times by various
-// services without waiting for network latency each time if cache was hit.
-//
-// The version typically doesn't matter in any way, but it is still recommended
-// to specify [protocol.CurrentVersion] to keep up the compatibility with the
-// `protocol` package.
-func Discover(appType, version string) (*Discovery, error) {
-	cacheMu.Lock()
-	defer cacheMu.Unlock()
-	requestURL := discoveryURL.JoinPath("/api/v1.0/discovery", appType, "builds", version).String()
-	if d, ok := cache[requestURL]; ok {
-		return d, nil
-	}
-
-	req, err := http.NewRequest(http.MethodGet, requestURL, nil)
-	if err != nil {
-		return nil, fmt.Errorf("make request: %w", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("User-Agent", internal.UserAgent)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return nil, internal.Err(resp)
-	}
-
-	var result internal.Result[Discovery]
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		return nil, fmt.Errorf("decode response body: %w", err)
-	}
-	d := &result.Data
-	cache[requestURL] = d
-	return d, nil
+// Environment represents the configuration for a network service in Minecraft: Bedrock Edition.
+// Each service must implement Environment as a struct representing a JSON object
+// to be decoded from the nested value from [Discovery.ServiceEnvironments].
+type Environment interface {
+	// ServiceName returns the name of the service.
+	// It is used as the top-most keys of [Discovery.ServiceEnvironments].
+	ServiceName() string
 }
-
-const (
-	// ApplicationTypeDedicatedServer is the application type for Bedrock Dedicated Server.
-	// distributed by Mojang/Microsoft. For only usage for listeners, it might be preferred
-	// over ApplicationTypeMinecraftPE.
-	ApplicationTypeDedicatedServer = "MinecraftDedicatedServer"
-
-	// ApplicationTypeMinecraftPE is the application type for Minecraft: Bedrock Edition
-	// clients on all platform.
-	//
-	// ApplicationTypeMinecraftPE should be the default application type used in most services
-	// as indicates both server and client.
-	ApplicationTypeMinecraftPE = "MinecraftPE"
-)
-
-var (
-	// cache is a map where keys are the string composed of <appType>-<version>
-	// and the values are the result of [Discover], cached to reduce network time.
-	cache = make(map[string]*Discovery)
-	// cacheMu is a mutex that should be locked when cache is in access.
-	cacheMu sync.Mutex
-
-	// discoveryURL is the base URL used to make a GET request for obtaining a Discovery
-	// from [Discover] with a specific application type and build version.
-	discoveryURL = &url.URL{
-		Scheme: "https",
-		Host:   "client.discovery.minecraft-services.net",
-	}
-)
 
 // Discovery provides environments for various services for Minecraft: Bedrock Edition.
 //
@@ -120,11 +56,75 @@ func (d *Discovery) Environment(env Environment) error {
 	return json.Unmarshal(m2, env)
 }
 
-// Environment represents the configuration for a network service in Minecraft: Bedrock Edition.
-// Each service must implement Environment as a struct representing a JSON object
-// to be decoded from the nested value from [Discovery.ServiceEnvironments].
-type Environment interface {
-	// ServiceName returns the name of the service.
-	// It is used as the top-most keys of [Discovery.ServiceEnvironments].
-	ServiceName() string
+var (
+	// cache is a map where keys are the string composed of <appType>-<version>
+	// and the values are the result of [Discover], cached to reduce network time.
+	cache = make(map[string]*Discovery)
+	// cacheMu is a mutex that should be locked when cache is in access.
+	cacheMu sync.Mutex
+
+	// discoveryURL is the base URL used to make a GET request for obtaining a Discovery
+	// from [Discover] with a specific application type and build version.
+	discoveryURL = &url.URL{
+		Scheme: "https",
+		Host:   "client.discovery.minecraft-services.net",
+	}
+)
+
+const (
+	// ApplicationTypeDedicatedServer is the application type for Bedrock Dedicated Server.
+	// distributed by Mojang/Microsoft. For only usage for listeners, it might be preferred
+	// over ApplicationTypeMinecraftPE.
+	ApplicationTypeDedicatedServer = "MinecraftDedicatedServer"
+
+	// ApplicationTypeMinecraftPE is the application type for Minecraft: Bedrock Edition
+	// clients on all platform.
+	//
+	// ApplicationTypeMinecraftPE should be the default application type used in most services
+	// as indicates both server and client.
+	ApplicationTypeMinecraftPE = "MinecraftPE"
+)
+
+// Discover obtains a Discover for the specific version for the specific
+// application type. The returned Discovery contains environments for
+// various services in Minecraft: Bedrock Edition and will be used as
+// the configuration for each service.
+//
+// The version typically doesn't matter in any way, but it is still recommended
+// to specify [protocol.CurrentVersion] to keep up the compatibility with the
+// `protocol` package.
+//
+// Discover caches the result and can be called multiple times by various
+// services without waiting for network latency each time if cache was hit.
+func Discover(appType, version string) (*Discovery, error) {
+	cacheMu.Lock()
+	defer cacheMu.Unlock()
+	requestURL := discoveryURL.JoinPath("/api/v1.0/discovery", appType, "builds", version).String()
+	if d, ok := cache[requestURL]; ok {
+		return d, nil
+	}
+
+	req, err := http.NewRequest(http.MethodGet, requestURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("make request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("User-Agent", internal.UserAgent)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, internal.Err(resp)
+	}
+
+	var result internal.Result[Discovery]
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, fmt.Errorf("decode response body: %w", err)
+	}
+	d := &result.Data
+	cache[requestURL] = d
+	return d, nil
 }
