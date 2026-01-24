2f6a161550f06c3a6b39ad8fae0840b66529f2e1
HashimTheArab <hashimssoftware@gmail.com>
2026-01-24 20:22:06 +0300

    perf: significant performance improvement and fix issue

--- minecraft/conn.go
+++ minecraft/conn.go
@@ -126,6 +126,7 @@ type Conn struct {
 	readDeadline    <-chan time.Time
 
 	sendMu sync.Mutex
+	encMu  sync.Mutex
 	// bufferedSend is a slice of byte slices containing packets that are 'written'. They are buffered until
 	// they are sent each 20th of a second.
 	bufferedSend [][]byte
@@ -366,11 +367,12 @@ func (conn *Conn) WritePacket(pk packet.Packet) error {
 		internal.BufferPool.Put(buf)
 	}()
 
-	conn.hdr.PacketID = pk.ID()
-	_ = conn.hdr.Write(buf)
-	l := buf.Len()
-
 	for _, converted := range conn.proto.ConvertFromLatest(pk, conn) {
+		buf.Reset()
+		conn.hdr.PacketID = converted.ID()
+		_ = conn.hdr.Write(buf)
+		l := buf.Len()
+
 		converted.Marshal(conn.proto.NewWriter(buf, conn.shieldID.Load()))
 
 		if conn.packetFunc != nil {
@@ -491,22 +493,28 @@ func (conn *Conn) Flush() error {
 		return conn.closeErr("flush")
 	default:
 	}
-	conn.sendMu.Lock()
-	defer conn.sendMu.Unlock()
 
-	if len(conn.bufferedSend) > 0 {
-		if err := conn.enc.Encode(conn.bufferedSend); err != nil && !errors.Is(err, net.ErrClosed) {
-			// Should never happen.
-			panic(fmt.Errorf("error encoding packet batch: %w", err))
-		}
-		// First manually clear out conn.bufferedSend so that re-using the slice after resetting its length to
-		// 0 doesn't result in an 'invisible' memory leak.
-		for i := range conn.bufferedSend {
-			conn.bufferedSend[i] = nil
-		}
-		// Slice the conn.bufferedSend to a length of 0 so we don't have to re-allocate space in this slice
-		// every time.
-		conn.bufferedSend = conn.bufferedSend[:0]
+	conn.sendMu.Lock()
+	if len(conn.bufferedSend) == 0 {
+		conn.sendMu.Unlock()
+		return nil
+	}
+	toSend := append([][]byte(nil), conn.bufferedSend...)
+	// First manually clear out conn.bufferedSend so that re-using the slice after resetting its length to
+	// 0 doesn't result in an 'invisible' memory leak.
+	for i := range conn.bufferedSend {
+		conn.bufferedSend[i] = nil
+	}
+	// Slice the conn.bufferedSend to a length of 0 so we don't have to re-allocate space in this slice
+	// every time.
+	conn.bufferedSend = conn.bufferedSend[:0]
+	conn.sendMu.Unlock()
+
+	conn.encMu.Lock()
+	defer conn.encMu.Unlock()
+	if err := conn.enc.Encode(toSend); err != nil && !errors.Is(err, net.ErrClosed) {
+		// Should never happen.
+		panic(fmt.Errorf("error encoding packet batch: %w", err))
 	}
 	return nil
 }
@@ -752,7 +760,9 @@ func (conn *Conn) handleRequestNetworkSettings(pk *packet.RequestNetworkSettings
 		return fmt.Errorf("send NetworkSettings: %w", err)
 	}
 	_ = conn.Flush()
+	conn.encMu.Lock()
 	conn.enc.EnableCompression(conn.compression, conn.compressionThreshold)
+	conn.encMu.Unlock()
 	conn.dec.EnableCompression(conn.compression, conn.maxDecompressedLen)
 	return nil
 }
@@ -763,7 +773,9 @@ func (conn *Conn) handleNetworkSettings(pk *packet.NetworkSettings) error {
 	if !ok {
 		return fmt.Errorf("unknown compression algorithm %v", pk.CompressionAlgorithm)
 	}
+	conn.encMu.Lock()
 	conn.enc.EnableCompression(alg, int(pk.CompressionThreshold))
+	conn.encMu.Unlock()
 	conn.dec.EnableCompression(alg, conn.maxDecompressedLen)
 	conn.readyToLogin = true
 	return nil
@@ -865,7 +877,9 @@ func (conn *Conn) handleServerToClientHandshake(pk *packet.ServerToClientHandsha
 	keyBytes := sha256.Sum256(append(salt, sharedSecret...))
 
 	// Finally we enable encryption for the enc and dec using the secret pubKey bytes we produced.
+	conn.encMu.Lock()
 	conn.enc.EnableEncryption(keyBytes)
+	conn.encMu.Unlock()
 	conn.dec.EnableEncryption(keyBytes)
 
 	// We write a ClientToServerHandshake packet (which has no payload) as a response.
@@ -1410,7 +1424,9 @@ func (conn *Conn) enableEncryption(clientPublicKey *ecdsa.PublicKey) error {
 	keyBytes := sha256.Sum256(append(conn.salt, sharedSecret...))
 
 	// Finally we enable encryption for the encoder and decoder using the secret key bytes we produced.
+	conn.encMu.Lock()
 	conn.enc.EnableEncryption(keyBytes)
+	conn.encMu.Unlock()
 	conn.dec.EnableEncryption(keyBytes)
 
 	return nil
