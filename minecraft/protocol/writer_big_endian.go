132161e90c261cf0cd1c21e5e9cf778abe0d6ba6
cqdetdev <101936396+cqdetdev@users.noreply.github.com>
2026-05-27 17:08:32 -0400

    minecraft: Remove scalar reads/writes from heap allocations (#429)

--- minecraft/protocol/writer_big_endian.go
+++ minecraft/protocol/writer_big_endian.go
@@ -5,67 +5,67 @@ package protocol
 import (
 	"encoding/binary"
 	"math"
-	"unsafe"
 )
 
 // Uint16 writes a little endian uint16 to the underlying buffer.
 func (w *Writer) Uint16(x *uint16) {
-	data := make([]byte, 2)
+	data := w.buf[:2]
 	binary.LittleEndian.PutUint16(data, *x)
 	_, _ = w.w.Write(data)
 }
 
 // Int16 writes a little endian int16 to the underlying buffer.
 func (w *Writer) Int16(x *int16) {
-	data := make([]byte, 2)
+	data := w.buf[:2]
 	binary.LittleEndian.PutUint16(data, uint16(*x))
 	_, _ = w.w.Write(data)
 }
 
 // Uint32 writes a little endian uint32 to the underlying buffer.
 func (w *Writer) Uint32(x *uint32) {
-	data := make([]byte, 4)
+	data := w.buf[:4]
 	binary.LittleEndian.PutUint32(data, *x)
 	_, _ = w.w.Write(data)
 }
 
 // Int32 writes a little endian int32 to the underlying buffer.
 func (w *Writer) Int32(x *int32) {
-	data := make([]byte, 4)
+	data := w.buf[:4]
 	binary.LittleEndian.PutUint32(data, uint32(*x))
 	_, _ = w.w.Write(data)
 }
 
 // BEInt32 writes a big endian int32 to the underlying buffer.
 func (w *Writer) BEInt32(x *int32) {
-	data := *(*[4]byte)(unsafe.Pointer(x))
-	_, _ = w.w.Write(data[:])
+	data := w.buf[:4]
+	binary.BigEndian.PutUint32(data, uint32(*x))
+	_, _ = w.w.Write(data)
 }
 
 // Uint64 writes a little endian uint64 to the underlying buffer.
 func (w *Writer) Uint64(x *uint64) {
-	data := make([]byte, 8)
+	data := w.buf[:8]
 	binary.LittleEndian.PutUint64(data, *x)
 	_, _ = w.w.Write(data)
 }
 
 // Int64 writes a little endian int64 to the underlying buffer.
 func (w *Writer) Int64(x *int64) {
-	data := make([]byte, 8)
+	data := w.buf[:8]
 	binary.LittleEndian.PutUint64(data, uint64(*x))
 	_, _ = w.w.Write(data)
 }
 
 // Float32 writes a little endian float32 to the underlying buffer.
 func (w *Writer) Float32(x *float32) {
-	data := make([]byte, 4)
+	data := w.buf[:4]
 	binary.LittleEndian.PutUint32(data, math.Float32bits(*x))
 	_, _ = w.w.Write(data)
 }
 
 // Float64 writes a little endian float64 to the underlying buffer.
 func (w *Writer) Float64(x *float64) {
-	data := make([]byte, 8)
+	data := w.buf[:8]
 	binary.LittleEndian.PutUint64(data, math.Float64bits(*x))
 	_, _ = w.w.Write(data)
 }
