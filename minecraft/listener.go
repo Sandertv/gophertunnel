aa5647fca37b4488d9ec8d55740121ba5f3de9d0
HashimTheArab <hashimssoftware@gmail.com>
2026-08-15 13:46:34 +0300

    minecraft: correct server-list pong field order

--- minecraft/listener.go
+++ minecraft/listener.go
@@ -372,14 +372,9 @@ func (listener *Listener) updatePongData() {
 	}); ok {
 		port = a.AddrPort().Port()
 	}
-	authOnline, authOffline := "0", "1"
-	if !listener.cfg.AuthenticationDisabled {
-		authOnline, authOffline = "1", "0"
-	}
-	listener.listener.PongData([]byte(fmt.Sprintf("MCPE;%v;%v;%v;%v;%v;%v;%v;%v;%v;%v;%v;%v;%v;%v;%v;",
+	listener.listener.PongData([]byte(fmt.Sprintf("MCPE;%v;%v;%v;%v;%v;%v;%v;%v;%v;%v;%v;%v;%v;",
 		s.ServerName, protocol.CurrentProtocol, protocol.CurrentVersion, s.PlayerCount, s.MaxPlayers,
-		listener.listener.ID(), s.ServerSubName, "Creative", 1, "1", port, port, "0", authOnline,
-		authOffline,
+		listener.listener.ID(), s.ServerSubName, "Creative", 1, port, port, 0, 0,
 	)))
 }
 
