7f058e5ddc393eaa0480dae338c5eee2feb323e6
هـ <104190360+didntpot@users.noreply.github.com>
2026-08-20 17:09:43 +0100

    gophertunnel: Updated for 1.26.45 (#511)

--- minecraft/protocol/info.go
+++ minecraft/protocol/info.go
@@ -2,7 +2,7 @@ package protocol
 
 const (
 	// CurrentProtocol is the current protocol version for the version below.
-	CurrentProtocol = 2168
+	CurrentProtocol = 2169
 	// CurrentVersion is the current version of Minecraft as supported by the `packet` package.
-	CurrentVersion = "1.26.44"
+	CurrentVersion = "1.26.45"
 )
